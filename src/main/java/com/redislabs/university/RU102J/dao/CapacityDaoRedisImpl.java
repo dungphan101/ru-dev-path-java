da80a3b94350a3557d2b2c58b4d9e2b18c7ac897
bee.dptm <ptmydung101097@gmail.com>
2025-12-05 10:42:11 +0700

    challenge training

--- src/main/java/com/redislabs/university/RU102J/dao/CapacityDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/CapacityDaoRedisImpl.java
@@ -58,8 +58,9 @@ public class CapacityDaoRedisImpl implements CapacityDao {
     // Challenge #4
     @Override
     public Long getRank(Long siteId) {
-        // START Challenge #4
-        return -2L;
-        // END Challenge #4
+        String capacityRankingKey = RedisSchema.getCapacityRankingKey();
+        try (Jedis jedis = jedisPool.getResource()) {
+            return jedis.zrevrank(capacityRankingKey, String.valueOf(siteId));
+        }
     }
 }
